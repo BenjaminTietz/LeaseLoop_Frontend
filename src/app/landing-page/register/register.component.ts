c9070a1aa0c6f8aa05025766e9a45cf0cfd88c0d
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-04-01 15:30:03 +0200

    refactor: change project structure and move services into app folder

--- src/app/landing-page/register/register.component.ts
+++ src/app/landing-page/register/register.component.ts
@@ -1,20 +1,20 @@
 import { Component, inject, signal } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
-import { AuthService } from '../../shared/services/auth/auth.service';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [MatIcon],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-  auth = inject(AuthService)
+  auth = inject(AuthService);
 
-  passVisible = signal(false)
+  passVisible = signal(false);
 
   togglePassVisible() {
-    this.passVisible.set(!this.passVisible())
+    this.passVisible.set(!this.passVisible());
   }
 }
--- src/app/shared/components/landing-header/landing-header.component.ts
+++ src/app/shared/components/landing-header/landing-header.component.ts
@@ -1,14 +1,14 @@
-import { Component, inject} from '@angular/core';
-import { NavigatorService } from '../../services/navigator/navigator.service';
+import { Component, inject } from '@angular/core';
+import { NavigatorService } from '../../../services/navigator/navigator.service';
 
 @Component({
   selector: 'app-landing-header',
   standalone: true,
   imports: [],
   templateUrl: './landing-header.component.html',
-  styleUrl: './landing-header.component.scss'
+  styleUrl: './landing-header.component.scss',
 })
 export class LandingHeaderComponent {
-  navigator = inject(NavigatorService)
+  navigator = inject(NavigatorService);
   constructor() {}
 }
