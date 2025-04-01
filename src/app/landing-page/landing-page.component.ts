54adf0d9f35699c8ff3bf3abe23b04dba2114d78
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-04-01 15:39:27 +0200

    refactor: structure of shared folder divide into landing / global & dashboard

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LandingFooterComponent } from "../shared/components/landing-footer/landing-footer.component";
-import { LandingHeaderComponent } from "../shared/components/landing-header/landing-header.component";
-
+import { LandingFooterComponent } from '../shared/landing-components/landing-footer/landing-footer.component';
+import { LandingHeaderComponent } from '../shared/landing-components/landing-header/landing-header.component';
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [RouterOutlet, LandingFooterComponent, LandingHeaderComponent],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
-
-}
+export class LandingPageComponent {}
