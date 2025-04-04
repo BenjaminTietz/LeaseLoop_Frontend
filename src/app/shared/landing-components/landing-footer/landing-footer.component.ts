51146d399e040cdf284b5f7665e4212498081837
NaviPlau <naviplau@outlook.com>
2025-04-04 18:26:20 +0200

    chore: style legal/nagigator-back

--- src/app/shared/landing-components/landing-footer/landing-footer.component.ts
+++ src/app/shared/landing-components/landing-footer/landing-footer.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { NavigatorService } from '../../../services/navigator/navigator.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-footer.component.html',
   styleUrl: './landing-footer.component.scss',
 })
--- src/app/shared/landing-components/landing-header/landing-header.component.ts
+++ src/app/shared/landing-components/landing-header/landing-header.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { NavigatorService } from '../../../services/navigator/navigator.service';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-landing-header',
   standalone: true,
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './landing-header.component.html',
   styleUrl: './landing-header.component.scss',
 })
