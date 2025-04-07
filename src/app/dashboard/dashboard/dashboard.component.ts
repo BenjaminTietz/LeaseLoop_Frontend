79abc95eb2e820760f6e18c29362ebfa5d0be2b5
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-04-07 14:36:22 +0200

    refactor: implemnt signals to dynamically render analytics in dashbaord

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -7,7 +7,6 @@ import { SidenavToggleComponent } from '../../shared/dashboard-components/sidena
 import { animate, style, transition, trigger } from '@angular/animations';
 import { ClickOutsideDirective } from '../../directives/outside-click/click-outside.directive';
 
-
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -17,21 +16,27 @@ import { ClickOutsideDirective } from '../../directives/outside-click/click-outs
     CommonModule,
     ThemeButtonComponent,
     SidenavToggleComponent,
-    ClickOutsideDirective
-],
+    ClickOutsideDirective,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
   animations: [
     trigger('fadeAnimation', [
       transition(':enter', [
         style({ opacity: 0, transform: 'translateX(-100%)' }),
-        animate('250ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+        animate(
+          '250ms ease-out',
+          style({ opacity: 1, transform: 'translateX(0)' })
+        ),
       ]),
       transition(':leave', [
-        animate('200ms ease-in', style({ opacity: 0, transform: 'translateX(-100%)' }))
-      ])
-    ])
-  ]
+        animate(
+          '200ms ease-in',
+          style({ opacity: 0, transform: 'translateX(-100%)' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class DashboardComponent {
   isSidebarOpen = signal(false);
--- src/app/dashboard/overview/overview.component.ts
+++ src/app/dashboard/overview/overview.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AvailabilityCalendarComponent } from '../../shared/dashboard-components/availability-calendar/availability-calendar.component';
+import { UnitsService } from '../../services/units-service/units.service';
+import { DashboardService } from '../../services/dashboard-service/dashboard.service';
 
 @Component({
   selector: 'app-overview',
@@ -8,4 +10,7 @@ import { AvailabilityCalendarComponent } from '../../shared/dashboard-components
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss',
 })
-export class OverviewComponent {}
+export class OverviewComponent {
+  unitService = inject(UnitsService);
+  dashboardService = inject(DashboardService);
+}
