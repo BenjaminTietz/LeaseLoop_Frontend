98a6a0b78d24aa9db03041ae60b4c3e696a93661
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-04-14 16:40:03 +0200

    chore: create invoice service

--- src/app/dashboard/billing/billing.component.ts
+++ src/app/dashboard/billing/billing.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { InvoiceService } from '../../services/invoice-service/invoice.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-billing',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './billing.component.html',
-  styleUrl: './billing.component.scss'
+  styleUrl: './billing.component.scss',
 })
-export class BillingComponent {
+export class BillingComponent implements OnInit {
+  invoiceServie = inject(InvoiceService);
 
+  ngOnInit(): void {
+    this.invoiceServie.getInvoices();
+  }
 }
