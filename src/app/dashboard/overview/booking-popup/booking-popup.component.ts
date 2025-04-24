89eaf31b052a111b2e7fc4d432ef298e8d0bf64b
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-04-24 18:45:26 +0200

    fix: show no services when not present in booking & added click outside

--- src/app/dashboard/overview/booking-popup/booking-popup.component.ts
+++ src/app/dashboard/overview/booking-popup/booking-popup.component.ts
@@ -1,14 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { DashboardService } from '../../../services/dashboard-service/dashboard.service';
 import { MatIcon } from '@angular/material/icon';
+import { ClickOutsideDirective } from '../../../directives/outside-click/click-outside.directive';
 
 @Component({
   selector: 'app-booking-popup',
   standalone: true,
-  imports: [MatIcon],
+  imports: [MatIcon, ClickOutsideDirective],
   templateUrl: './booking-popup.component.html',
   styleUrl: './booking-popup.component.scss',
 })
 export class BookingPopupComponent {
   dashboardService = inject(DashboardService);
+
+  closePopup = () => {
+    this.dashboardService.isbookingPopupOpen.set(false);
+  };
 }
