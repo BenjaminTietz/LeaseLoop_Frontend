124d24b50b05433297d2ec41d6a580341abb610c
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-06-23 17:45:57 +0200

    refactor: delete unused imports, formated code & add jsdoc to complete invoices & its components

--- src/app/dashboard/invoices/invoices.component.ts
+++ src/app/dashboard/invoices/invoices.component.ts
@@ -1,16 +1,22 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { InvoiceService } from '../../services/invoice-service/invoice.service';
 import { CommonModule } from '@angular/common';
-import { ProgressBarComponent } from "../../shared/global/progress-bar/progress-bar.component";
-import { PagingComponent } from "../../shared/dashboard-components/paging/paging.component";
-import { SearchInputComponent } from "../../shared/dashboard-components/search-input/search-input.component";
+import { ProgressBarComponent } from '../../shared/global/progress-bar/progress-bar.component';
+import { PagingComponent } from '../../shared/dashboard-components/paging/paging.component';
+import { SearchInputComponent } from '../../shared/dashboard-components/search-input/search-input.component';
 import { environment } from '../../../environments/environment';
-import { FilterComponent } from "../../shared/global/filter/filter.component";
+import { FilterComponent } from '../../shared/global/filter/filter.component';
 
 @Component({
   selector: 'app-invoices',
   standalone: true,
-  imports: [CommonModule, ProgressBarComponent, PagingComponent, SearchInputComponent, FilterComponent],
+  imports: [
+    CommonModule,
+    ProgressBarComponent,
+    PagingComponent,
+    SearchInputComponent,
+    FilterComponent,
+  ],
   templateUrl: './invoices.component.html',
   styleUrl: './invoices.component.scss',
 })
@@ -24,22 +30,34 @@ export class InvoicesComponent implements OnInit {
     { label: 'Name (Z-A)', value: 'descending_name' },
     { label: 'Date (latest) ', value: 'ascending_date' },
     { label: 'Date (earliest)', value: 'descending_date' },
-    { label: 'Booking ID (lowest)', value: 'booking_id_lowest' }, 
+    { label: 'Booking ID (lowest)', value: 'booking_id_lowest' },
     { label: 'Booking ID (highest)', value: 'booking_id_highest' },
     { label: 'Amount (lowest)', value: 'amount_lowest' },
-    { label: 'Amount (highest)', value: 'amount_highest' }
-  ]
+    { label: 'Amount (highest)', value: 'amount_highest' },
+  ];
 
+  /**
+   * Lifecycle hook that is called after the component is initialized.
+   * It triggers the loading of the invoices from the server.
+   */
   ngOnInit(): void {
     this.invoiceService.getInvoices();
   }
 
+  /**
+   * Searches for invoices based on the given search term.
+   * @param searchTerm The search term to search for.
+   */
   search(searchTerm: string) {
     this.searchInput.set(searchTerm);
     this.invoiceService.getInvoices(1, searchTerm);
   }
 
-  filterInvoices(filter:string){
+  /**
+   * Filters the invoices based on the provided filter criteria.
+   * @param filter The filter criteria to apply.
+   */
+  filterInvoices(filter: string) {
     this.invoiceService.filterValue.set(filter);
     this.invoiceService.getInvoices(1, this.searchInput());
   }
