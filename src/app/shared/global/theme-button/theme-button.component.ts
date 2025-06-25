83b59c17d7a615544da1ba3c21d06733994ef884
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-06-25 10:35:07 +0200

    refactor: format code delete unused imports and add jsdoc to all global components

--- src/app/shared/global/theme-button/theme-button.component.ts
+++ src/app/shared/global/theme-button/theme-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ThemeService } from '../../../services/theme-service/theme.service';
 import { MatIcon } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
@@ -15,13 +15,19 @@ export class ThemeButtonComponent {
   themeService = inject(ThemeService);
   navigator = inject(NavigatorService);
 
-
-
+  /**
+   * Initializes the theme settings on component initialization.
+   * Calls the theme service to set the initial theme and updates the current theme state.
+   */
   ngOnInit() {
     this.themeService.initTheme();
-    this.themeService.currentTheme.set(this.themeService.getCurrentTheme())
+    this.themeService.currentTheme.set(this.themeService.getCurrentTheme());
   }
 
+  /**
+   * Toggles the application theme between light and dark.
+   * This method calls the theme service to toggle the theme and then updates the component's current theme state.
+   */
   toggleTheme(): void {
     this.themeService.toggleTheme();
     this.themeService.currentTheme.set(this.themeService.getCurrentTheme());
